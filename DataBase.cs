981188231784ace8ebfe3c9ef5df4f1eb0d87bc5
Rakan8181 <112816380+Rakan8181@users.noreply.github.com>
2023-05-25 13:23:18 +0100

    Add project files.

---
+++ DataBase.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using System.Data;
+using System.Reflection;
+
+namespace StudentsDatabase
+{
+    internal class DataBase
+    {
+        private string _connectionString;
+
+        public DataBase(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public List<string> GetClasses(string subject)
+        {
+            List<string> classes = new List<string>();
+            using (SqliteConnection connection = new SqliteConnection())
+            {
+                connection.ConnectionString = _connectionString;
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "select * from Class join Courses on Class.CourseID = Courses.CourseID where Courses.Title = (@Name);";
+                var nameParameter = command.Parameters.Add("@Name", SqliteType.Text);
+                nameParameter.Value = subject;
+                var dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    var name = dataReader.GetString(0);
+                    classes.Add(name);
+                }
+                return classes;
+
+            }
+        }
+        public List<string> StudentsinClass(string _class)
+        {
+            List<string> students = new List<string>();
+            using (SqliteConnection connection = new SqliteConnection())
+            {
+                connection.ConnectionString = _connectionString;
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "select s.FirstName,s.SecondName from ClassAttendance c join Students s on c.StudentID = s.StudentID where c.Class = (@Name);";
+                var nameParameter = command.Parameters.Add("@Name", SqliteType.Text);
+                nameParameter.Value = _class;
+                var dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    var student = dataReader.GetString(0);
+                    students.Add(student);
+                }
+                return students;
+            }
+        }
+        public List<string> StudentOptions(string secondName)
+        {
+            List<string> classes = new List<string>();
+            using (SqliteConnection connection = new SqliteConnection())
+            {
+                connection.ConnectionString = _connectionString;
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "select c.Class from Students s join ClassAttendance c on s.StudentID = c.StudentID where s.SecondName = (@Name);";
+                var nameParameter = command.Parameters.Add("@Name", SqliteType.Text);
+                nameParameter.Value = secondName;
+                var dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    var _class = dataReader.GetString(0);
+                    classes.Add(_class);
+                    
+                }
+                return classes;
+
+            }
+        }
+        public int GetStudentID(string secondName)
+        {
+            using (SqliteConnection connection = new SqliteConnection())
+            {
+                connection.ConnectionString = _connectionString;
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "select StudentID from Students where SecondName = (@Name) ";
+                var nameParameter = command.Parameters.Add("@Name", SqliteType.Text);
+                nameParameter.Value = secondName;
+                var dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    string value = dataReader.GetString(0);
+                    int StudentID = int.Parse(value);
+                    return StudentID;
+                }
+                return (-1);
+            }
+        }
+        public void AddStudenttoClass(string secondName, string _class)
+        {
+            int StudentID = GetStudentID(secondName);
+            using (SqliteConnection connection = new SqliteConnection())
+            {
+                connection.ConnectionString = _connectionString;
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "insert into ClassAttendance (Class,StudentID) values ((@Class),(@StudentID))";
+                var nameParameter1 = command.Parameters.Add("@Class", SqliteType.Text);
+                nameParameter1.Value = _class;
+                var nameParameter2 = command.Parameters.Add("@StudentID", SqliteType.Text);
+                nameParameter2.Value = StudentID;
+                command.ExecuteNonQuery();
+                Console.WriteLine($"{secondName} has been added to {_class}");
+            }
+        }
+        public void RemoveStudentfromClass(string secondName, string _class)
+        {
+            int StudentID = GetStudentID(secondName);
+            using (SqliteConnection connection = new SqliteConnection())
+            {
+                connection.ConnectionString = _connectionString;
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = "delete from ClassAttendance where StudentID = (@StudentID) and Class = (@Class);";
+                var nameParameter1 = command.Parameters.Add("@StudentID", SqliteType.Text);
+                nameParameter1.Value = StudentID;
+                var nameParameter2 = command.Parameters.Add("@Class", SqliteType.Text);
+                nameParameter2.Value = _class;
+                command.ExecuteNonQuery();
+                Console.WriteLine($"{secondName} has been removed from {_class}");
+            }
+        }
+        public void ValidateStudentClasses(string secondName)
+        {
+            List<string> mfl = GetClasses("Spanish");
+            mfl.AddRange(GetClasses("French"));
+            List<string> humanities = GetClasses("Geography");
+            humanities.AddRange(GetClasses("History"));
+            List<string> stem = GetClasses("Maths");
+            stem.AddRange(GetClasses("Computing"));
+            stem.AddRange(GetClasses("Economics"));
+
+            //must have 1 mfl, 1 humanity, and 1 stem
+            int StudentID = GetStudentID(secondName);
+            List<string> classes = StudentOptions(secondName);
+            bool humanity = false;
+            bool stem_ = false;
+            bool language = false;
+            foreach (string className in classes)
+            {
+                Console.WriteLine(className);
+                if (mfl.Contains(className)){
+                    language = true;
+                }
+                else if (humanities.Contains(className))
+                {
+                    humanity = true;
+                }
+                else if (stem.Contains(className))
+                {
+                    stem_ = true;
+                }
+            }
+            int numSubjects = classes.Count();
+            bool num = false;
+            if (numSubjects > 4) {
+                num = true;
+            }
+
+            if (language &&  stem_ && humanity && num) {
+                Console.WriteLine("Valid options");
+                
+            }
+            else
+            {
+                Console.WriteLine($"MFL: {language} ");
+                Console.WriteLine($"Humanity: {humanity}");
+                Console.WriteLine($"STEM: {stem_}");
+                Console.WriteLine($"Number of Subjects: {numSubjects}");
+                Console.WriteLine("All 3 need to be true as well as having at least 5 subjects");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿namespace StudentsDatabase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=C:\\Users\\44734\\source\\repos\\StudentsDatabase\\Student Database.db;Mode=ReadWrite;";
+            Menu(connectionString);
+
+
+        }
+        static void Menu(string connectionstring)
+        {
+            DataBase db = new(connectionstring);
+            bool test = true;
+            while (test)
+            {
+                Console.WriteLine("1: Get Classes for each course ");
+                Console.WriteLine("2: Get students for each class");
+                Console.WriteLine("3: Get classes for each student");
+                Console.WriteLine("4: Add a student to a class");
+                Console.WriteLine("5: Remove a student from a class");
+                Console.WriteLine("6: Validate a student's options");
+                Console.WriteLine("7: Exit");
+                string choice = Console.ReadLine();
+                
+                switch (choice)
+                {
+                    case ("1"):
+                        Console.WriteLine("Enter the course you would like to display the classes for: ");
+                        Console.WriteLine("Options: History,Computing,Maths,Economics,Geography,French,Spanish");
+                        string courseChoice = Console.ReadLine();
+                        List<string> classes = db.GetClasses(courseChoice);
+                        Console.WriteLine("Classes:");
+
+                        foreach (var class_ in classes)
+                        {
+                            Console.WriteLine(class_);
+                        }
+                        break;
+                    case ("2"):
+                        Console.WriteLine("Enter the class you want to display students in that class: ");
+                        Console.WriteLine("GCSE choices available: Maths GCSE, Computing GCSE, French GCSE, Spanish GCSE, History GCSE, Geography GCSE");
+                        Console.WriteLine("A Levels: Maths A Level,Computing A Level, French A Level, Spanish A Level, Economics A Level, Geography A Level ");
+                        Console.WriteLine("Uni Classes: Maths University");
+                        string _class = Console.ReadLine();
+                        List<string> students = db.StudentsinClass(_class);
+                        Console.WriteLine($"Classes: for {_class} ");
+                        foreach (var student in students)
+                        {
+                            Console.WriteLine(student);
+                        }
+                        break;
+
+                    case ("3"):
+                        Console.WriteLine("Enter the second name of the student, to display the classes for that student");
+                        string secondName = Console.ReadLine();
+                        List<string> options = db.StudentOptions(secondName);
+                        Console.WriteLine($"Classes: for {secondName} ");
+                        foreach (var option in options)
+                        {
+                            Console.WriteLine(option);
+                        }
+                        break;
+                    case ("4"):
+                        Console.WriteLine("Enter the second name of the student you would like to add to a class: ");
+                        string secondname = Console.ReadLine();
+                        Console.WriteLine("Enter the class: ");
+                        _class = Console.ReadLine();
+                        db.AddStudenttoClass(secondname, _class);
+                        break;
+                    case ("5"):
+                        Console.WriteLine("Enter the second name of the student you would like to remove from a class: ");
+                        secondname = Console.ReadLine();
+                        Console.WriteLine("Enter the class: ");
+                        _class = Console.ReadLine();
+                        db.RemoveStudentfromClass(secondname, _class);
+                        break;
+                    case ("6"):
+                        Console.WriteLine("Requirement: 1 Language, 1 Humanity, 1 STEM subject, 5 subjects");
+                        Console.WriteLine("Enter a second name to validate choice of classes: ");
+                        secondName = Console.ReadLine();
+                        db.ValidateStudentClasses(secondName);
+                        break;
+                    case ("7"):
+                        test = false;
+                        break;
+                    default:
+                        test = false;
+                        break;
+                    
+                }
+                Console.WriteLine();
+
+
+            }
+            
+
+            
+            //let a student add/delete a class
+        }
+    }
+}
